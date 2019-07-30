e812afad7b61b9f89328e2e2919f808082146789
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-07-30 07:32:29 -1000

    first commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+import router from './router'
+import store from './store'
+
+axios.defaults.baseURL = 'https://vueaxiosproject-4b90c.firebaseio.com';
+// axios.defaults.headers.common['Authorization'] = 'fasdfasdf';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
+const requestInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config)
+  return config
+})
+
+const responseInterceptor =axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res)
+  return res
+})
+
+axios.interceptors.request.eject(requestInterceptor)
+axios.interceptors.request.eject(responseInterceptor)
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
